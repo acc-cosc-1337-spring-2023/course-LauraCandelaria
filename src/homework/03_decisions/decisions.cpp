524d1f41fb641400d0a6a9dd9710dfdb959d58aa
LauraCandelaria <123007632+LauraCandelaria@users.noreply.github.com>
2023-02-17 20:33:28 +0000

    HW 3

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,68 @@
 //write include statement for decisions header
+#include<iostream>
+#include"decisions.h"
+using std::string;
 
 
 //Write code for function(s) code here
+string get_letter_grade_using_if(int grade)
+{
+    string result;
+
+    if(grade <= 100 && grade >= 90)
+    {
+        result = "A";
+    }
+    else if(grade <= 89 && grade >= 80)
+    {
+        result = "B";
+    }
+    else if(grade <= 79 && grade >= 70)
+    {
+        result = "C";
+    }
+    else if(grade <= 69 && grade >= 60)
+    {
+        result = "D";
+    }
+    else if(grade <= 59  && grade >= 0)
+    {
+        result = "F";
+    }
+    else
+    {
+        result = "Invalid grade";
+    }
+
+    return result;
+}
+
+
+string get_letter_grade_using_switch(int grade)
+{
+    string result;
+
+    switch(grade)
+    {
+        case 90 ... 100:
+            result = "A";
+            break;
+        case 80 ... 89:
+            result = "B";
+            break;
+        case 70 ... 79:
+            result = "C";
+            break;
+        case 60 ... 69:
+            result = "D";
+            break;
+        case 0 ... 59:
+            result = "F";
+            break;
+        default:
+            result = "Invalid grade";
+            
+    }
+
+    return result;
+}
