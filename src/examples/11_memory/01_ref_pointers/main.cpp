1d78292609730e012c1e3db720435f0c596ae23d
LauraCandelaria <123007632+LauraCandelaria@users.noreply.github.com>
2023-04-14 23:59:48 +0000

    example practice

--- src/examples/11_memory/01_ref_pointers/main.cpp
+++ src/examples/11_memory/01_ref_pointers/main.cpp
@@ -1,8 +1,28 @@
 #include "ref_pointers.h"
 #include<iostream>
+using std::cout;
 
 int main() 
 {
-	
+	/*
+	auto num = 5;
+	auto &num_ref = num;
+
+	cout<<"The value of num is: "<<num<<"\n";
+	cout<<"The address of num is: "<<&num<<"\n";
+	cout<<"The value of num_ref is: "<<num_ref<<"\n";
+	cout<<"The address of &num_ref is: "<<&num_ref<<"\n";
+	*/
+// pointers
+
+	auto num = 5, num1 = 10;
+	int *num_ptr = &num; // point to memory of num
+
+	cout<<"The value of num is: "<<num<<"\n";
+	cout<<"The address of num is: "<<&num<<"\n";
+	cout<<"The address of num_ptr is: "<<num_ptr<<"\n";
+	cout<<"The value of num_ptr is: "<<*num_ptr<<"\n";
+
+
 	return 0;
 }
